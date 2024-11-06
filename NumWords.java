ae7708dddc0c86e4daca16a3def335a14edc9dae
raaedj <raed.agbaria@post.runi.ac.il>
2024-11-06 19:32:21 +0200

    test num

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,12 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		int hundreds = num / 100;
+		int ones = num % 10;
+		int tens = (num - ones - (hundreds*100)) / 10;
+
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
+		
 	}
 }
