1fa23ba9df8bba0d9d578d8038eb5fc5a1664594
raaedj <raed.agbaria@post.runi.ac.il>
2024-11-06 15:54:21 +0200

    test Bill3

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		double value = Math.ceil(Double.parseDouble(args[3]) / 3);
-		System.out.println("Dear " + name3 + ", " + name2 + ", " + " and " + name1 +
+		System.out.println("Dear " + name3 + ", " + name2 + ", " + "and " + name1 +
 		                   ": pay " + value + " Shekels each");
 
 	    // Replace this comment with the rest of your code   
